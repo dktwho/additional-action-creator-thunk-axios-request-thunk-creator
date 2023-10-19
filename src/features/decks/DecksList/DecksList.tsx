b0e36439f5490c9730c55b7e5d382df20836d6b8
Alexander Korobov <dktwho@mail.ru>
2023-10-19 11:16:13 +0300

    create setDecksAC with autoType, add case

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,8 +1,12 @@
 import s from './DecksList.module.css'
 import { useEffect } from 'react'
 import { decksAPI } from '../decks-api.ts'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 export const DecksList = () => {
+  const decks = useSelector(state => state.decks)
+  const dispatch = useDispatch()
   useEffect(() => {
     decksAPI.fetchDecks()
   }, [])
--- src/features/decks/decks-reducer.ts
+++ src/features/decks/decks-reducer.ts
@@ -1,5 +1,7 @@
+import { Deck } from './decks-api.ts'
+
 const initialState = {
-  decks: [] as any[], // todo: add type
+  decks: [] as Deck[], // todo: add type
   searchParams: {
     name: '',
   },
@@ -7,6 +9,22 @@ const initialState = {
 
 type DecksState = typeof initialState
 
-export const decksReducer = (state: DecksState = initialState, action: any): DecksState => {
+export const decksReducer = (state: DecksState = initialState, action: DecksActions): DecksState => {
+  switch (action.type) {
+    case 'DECKS/SET-DECKS': {
+      return state
+    }
+    default : {
+      return { ...state, decks: action.decks }
+    }
+  }
   return state
 }
+
+type DecksActions = ReturnType<typeof setDecksAC>
+export const setDecksAC = (decks: Deck[]) => ({
+  type: 'DECKS/SET-DECKS' as const,
+  decks,
+})
+
+
